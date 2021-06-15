6c8f23047b7c384d6734a22f8b300a37af0a5cb8
saalajmi <saalajmi@tvtc.gov.sa>
2021-06-15 20:41:07 +0300

    adding comments

---
+++ Student.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace saalajmi
+{
+    public class Student
+    {
+        private string id;
+        private string name;
+        private string _class;
+        private string section;
+        public Student()
+        {
+        }
+
+        public string ID
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Class
+        {
+            get { return _class; }
+            set { _class = value; }
+        }
+
+        public string Section
+        {
+            get { return section; }
+            set { section = value; }
+        }
+    }
+}
